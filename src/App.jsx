0cffa7c17fe9753c9d5b3fae152a40130ee13f78
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-03-07 11:42:15 -0500

    Add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import Navigation from './components/Navigation';
 function App() {
   return (
     <div>
-     <h1 className="text-red-200">
-      Hello world!
-    </h1>
       <Navigation />
     </div>
   );
